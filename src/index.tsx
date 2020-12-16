20ff5ba5dbe474238ee11d0bdeb39f67ff0a1be2
Majd Shamma <majd.sh42@gmail.com>
2020-12-16 17:04:54 +0200

    Multilanguage setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Translation Hook
+import { setTranslations, setDefaultLanguage } from 'react-multi-lang'
+import en from './laguages/en.json'
+import ar from './laguages/ar.json'
+
+// Setting up translations
+setTranslations({en, ar})
+setDefaultLanguage('en')
 
 ReactDOM.render(
   <React.StrictMode>
