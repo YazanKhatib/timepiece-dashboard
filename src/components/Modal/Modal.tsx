cafa5c4ac9edb4d30aef577b6d7c0a3efab66361
Majd Shamma <majd.sh42@gmail.com>
2020-12-30 20:54:17 +0200

    Modal Component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+
+import './Modal.css'
+
+interface ModalProps {
+    open: boolean,
+    toggle: Function,
+    children?: any
+}
+
+export default (props: ModalProps) => {
+
+    const [out, setOut] = useState<boolean>(false)
+    const [dasharray, setDasharray] = useState<number>(0)
+    
+    const hide = () => {
+        setOut(true);
+        setTimeout(() => {
+            props.toggle()
+        }, 300);
+    }
+
+    return (
+        <>
+            { props.open ?
+                <div id="modal-container" className={out ? "out" : ""} onClick={hide}>
+                    <div className="modal-background">
+                        <div ref={(component: HTMLDivElement) => { if(component) setDasharray(component.offsetWidth * 2 + component.offsetHeight * 2) } } className="modal" onClick={(e: React.MouseEvent<HTMLDivElement>) => e.stopPropagation()}>
+                            <div className="modal-content">
+                                {props.children}
+                            </div>
+                            {dasharray ?
+                            <svg className="modal-svg" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none">
+                                <rect style={{ strokeDasharray: dasharray, strokeDashoffset: dasharray }} x="0" y="0" fill="none" width="226" height="162" rx="3" ry="3"></rect>
+                            </svg> : ""}
+                        </div>
+                    </div>
+                    <style>{`
+                        @keyframes sketchIn {
+                            0% {
+                                stroke-dashoffset: ${dasharray};
+                            }
+                            100% {
+                                stroke-dashoffset: 0;
+                            }
+                        }
+                        
+                        @keyframes sketchOut {
+                            0% {
+                                stroke-dashoffset: 0;
+                            }
+                            100% {
+                                stroke-dashoffset: ${dasharray};
+                            }
+                        }
+                        
+                        @keyframes sketchInDark {
+                            0% {
+                                stroke-dashoffset: ${dasharray};
+                            }
+                            100% {
+                                stroke-dashoffset: 0;
+                                stroke: #1a2029;
+                            }
+                        }
+                        
+                        @keyframes sketchOutDark {
+                            0% {
+                                stroke-dashoffset: 0;
+                                stroke: #1a2029;
+                            }
+                            100% {
+                                stroke-dashoffset: ${dasharray};
+                            }
+                        }
+                    `}</style>
+                </div> : ""}
+        </>
+    )
+
+}
